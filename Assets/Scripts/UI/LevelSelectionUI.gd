abb2199f8963c77748195ce756ed778db0c72339
ReviXy <gthxbr2004@gmail.com>
2025-11-03 00:42:44 +0300

    Merge and cleanup.

--- Assets/Scripts/UI/LevelSelectionUI.gd
+++ Assets/Scripts/UI/LevelSelectionUI.gd
@@ -1,26 +1,12 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_back_to_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Assets/Scenes/MainMenu.tscn")
 
-
-
 func _on_level_button_pressed(button) -> void:
 	GlobalLevelManager.levelID = (button as Button).text.to_int()
-	get_tree().change_scene_to_file("res://Assets/Scenes/TestLevel.tscn")
-
+	get_tree().change_scene_to_file("res://Assets/Scenes/Level.tscn")
 
 func _on_test_level_button_pressed() -> void:
 	GlobalLevelManager.levelID = -1
-	get_tree().change_scene_to_file("res://Assets/Scenes/TestLevel.tscn")
+	get_tree().change_scene_to_file("res://Assets/Scenes/Level.tscn")
--- Assets/Scripts/UI/MainMenuUI.gd
+++ Assets/Scripts/UI/MainMenuUI.gd
@@ -3,7 +3,6 @@ extends Control
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Assets/Scenes/LevelSelectionMenu.tscn")
 
-
 func _on_options_button_pressed() -> void:
 	SettingsManager.showSettingsMenu()
 
