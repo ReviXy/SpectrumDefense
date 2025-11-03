3fcdf3d4e538bb65c8edce327c0b0584a2e26113
Tlotro <misha.pletneow@yandex.ru>
2025-11-04 02:29:55 +0300

    Hotfix Status effects

--- Assets/Scripts/Effects/EnemyAttachment.gd
+++ Assets/Scripts/Effects/EnemyAttachment.gd
@@ -25,8 +25,8 @@ func pre_physics():
 ##baseDamage is the raw damage that would be dealt.
 ##preSum, mult and sum can be changed
 ##baseDamage can't and only serves to show how much damage would have been taken (shields)
-func pre_damage(_baseDamage: float, _color:Color, _preSum:Ref, _mult:Ref, _sum:Ref, _proceed:Ref):
-	pass
+func pre_damage(_baseDamage: float, _color:Color, _preSum:Ref, _mult:Ref, _sum:Ref):
+	return true
 
 func post_damage(_damage: float, _color: Color):
 	pass
